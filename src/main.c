a279fc286b7bdc49f1fe1cb4e49a6f8e5eb65c6d
Enchan <enchant0130@gmail.com>
2024-09-08 17:57:35 +0900

    [Update] EEEPROM reset sequence
    - リセットシーケンスの送信とリセットの確認

--- src/main.c
+++ src/main.c
@@ -1,19 +1,46 @@
-// template: blink LED at pin 22 with input from pin 21
+// Single-Wire EEPROM AT21CS01 sample
 #include <pico/stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
 
-const unsigned int LED_PIN = 22;
-const unsigned int SWITCH_PIN = 21;
+const unsigned int IO_PIN = 14;
 
 int main() {
-    // initialize GPIO
-    gpio_init(LED_PIN);
-    gpio_init(SWITCH_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_set_dir(SWITCH_PIN, GPIO_IN);
+    stdio_init_all();
+
+    gpio_init(IO_PIN);
+    gpio_set_dir(IO_PIN, GPIO_OUT);
+
+    // reset device
+    gpio_put(IO_PIN, false);
+    sleep_us(50);  // t_reset min@high: 48
+
+    gpio_set_dir(IO_PIN, GPIO_IN);
+    sleep_us(10);  // t_rrt min@high: 8
+
+    gpio_set_dir(IO_PIN, GPIO_OUT);
+    gpio_put(IO_PIN, false);
+    sleep_us(2);  // t_drr max@high: 2 - t_pup
+    gpio_set_dir(IO_PIN, GPIO_IN);
+
+    // ACK lasts up to 24us
+    uint8_t ackTimes = 30;
+    while (--ackTimes > 0 && gpio_get(IO_PIN) == true) {
+        // Actually, getting state from gpio requires more than 1us (I think), so this sleep won't be needed
+        sleep_us(1);
+    }
+
+    // Confirm ACK
+    if (ackTimes > 0) {
+        printf("ACK from EEPROM received\n");
+    } else {
+        printf("EEPROM reset timed out\n");
+        while (true) {
+        }
+    }
 
     // loop
     while (true) {
-        bool isPressed = !gpio_get(SWITCH_PIN);
-        gpio_put(LED_PIN, isPressed);
+        sleep_ms(1000);
     }
 }
