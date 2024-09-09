3d15aedf5643b209a80105b0a744636c64c48292
Enchan <enchant0130@gmail.com>
2024-09-09 19:47:05 +0900

    [Update] try to write command (not work)

--- src/main.c
+++ src/main.c
@@ -5,13 +5,8 @@
 
 const unsigned int IO_PIN = 14;
 
-int main() {
-    stdio_init_all();
-
-    gpio_init(IO_PIN);
+bool eeprom_reset() {
     gpio_set_dir(IO_PIN, GPIO_OUT);
-
-    // reset device
     gpio_put(IO_PIN, false);
     sleep_us(50);  // t_reset min@high: 48
 
@@ -29,14 +24,76 @@ int main() {
         // Actually, getting state from gpio requires more than 1us (I think), so this sleep won't be needed
         sleep_us(1);
     }
+    return ackTimes > 0;
+}
 
-    // Confirm ACK
-    if (ackTimes > 0) {
-        printf("ACK from EEPROM received\n");
+void eeprom_write_bit(bool value) {
+    // TODO: もう少し時間縮められるはず
+    const uint8_t tBit = 20;              // t_bit max@high: 25
+    const uint8_t tLow = value ? 1 : 10;  // t_low0 max@high: 16 t_low1 max@high: 2
+    gpio_set_dir(IO_PIN, GPIO_OUT);
+    gpio_put(IO_PIN, false);
+    sleep_us(tLow);
+    gpio_set_dir(IO_PIN, GPIO_IN);
+    sleep_us(tBit - tLow);
+}
+
+void eeprom_write_byte(uint8_t data) {
+    // MSB first
+    for (size_t i = 0; i < 8; i++) {
+        eeprom_write_bit(data & 1 << (7 - i));
+    }
+}
+
+bool eeprom_read_bit() {
+    const uint8_t tBit = 20;  // t_bit max@high: 25
+    const uint8_t tRd = 1;    // t_rd max@high: 2 - t_pup
+    const uint8_t tMrs = 2;   // t_mrs max@high: 2
+    gpio_set_dir(IO_PIN, GPIO_OUT);
+    gpio_put(IO_PIN, false);
+    sleep_us(tRd);
+    gpio_set_dir(IO_PIN, GPIO_IN);
+    sleep_us(tMrs - tRd);
+    bool result = gpio_get(IO_PIN);
+    sleep_us(tBit - tRd);
+    return result;
+}
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(IO_PIN);
+    gpio_disable_pulls(IO_PIN);
+
+    // Reset EEPROM
+    bool detected = eeprom_reset();
+    if (detected) {
+        printf("EEPROM successfully reset.\n");
     } else {
-        printf("EEPROM reset timed out\n");
-        while (true) {
-        }
+        printf("EEPROM reset timed out.\n");
+    }
+
+    // send command _Manufacturer ID Read_ (0x0C)
+    const uint8_t opCode = 0x0C;
+
+    // The slave address (A2-A0) cannot be confirmed unless SOIC or XSFN!
+
+    // start condition
+    gpio_set_dir(IO_PIN, GPIO_IN);
+    sleep_us(160);  // t_htss min@high: 150
+
+    const uint8_t deviceAddress = 0x00;
+
+    // <opcode><address><r/w#>
+    const uint8_t command = (opCode << 4) | ((deviceAddress & 0x07) << 1) | 0x01;
+    printf("Try to send %02X... ", command);
+    eeprom_write_byte(command);
+    printf("Sent.\n");
+    bool ack = eeprom_read_bit();
+    if (!ack) {
+        printf("Device reacted command. (attempted device address: %d)\n", deviceAddress);
+    } else {
+        printf("No response. (attempted device address: %d)\n", deviceAddress);
     }
 
     // loop
