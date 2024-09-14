2cb1eb96b093ff3d5122811e2957f9b096b439cf
Enchan <enchant0130@gmail.com>
2024-09-14 13:44:13 +0900

    [Update] Reset using PIO, get response
    - PIO側でリセットできるように
    - チップのレスポンスを受け取れるように
    - デバイス検索処理を実装

--- src/main.c
+++ src/main.c
@@ -9,11 +9,15 @@
 
 const unsigned int IO_PIN = 14;
 
+const uint8_t ACK = 0;
+const uint8_t NACK = 1;
+
+const uint8_t CommandManufacturerIDRead = 0x0C;
+
 int main() {
     stdio_init_all();
 
     const uint statemachineId = 0;
-
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &singlewireComm_program);
 
@@ -21,8 +25,41 @@ int main() {
     singlewireComm_program_init(pio, statemachineId, offset, IO_PIN);
     pio_sm_set_enabled(pio, statemachineId, true);
 
-    // スタックに積んだ瞬間に動き出す
-    pio_sm_put_blocking(pio, statemachineId, 0x5A);
+    // リセット要求を送り、結果を待つ
+    pio_sm_put_blocking(pio, statemachineId, 0xFFFFFFFF);
+    uint32_t resetResult = pio_sm_get_blocking(pio, statemachineId);
+    if (resetResult != 0) {
+        printf("EEPROM Reset failed\n");
+        while (true) {
+            sleep_ms(1000);
+        }
+    }
+    printf("EEPROM Reset successfully\n");
+
+    // オペコード 0x0C(チップ情報読み取り) を各スレーブアドレスに対して送信し、デバイスを探す
+    printf("Looking for connected devices...\n");
+    int deviceAddress = -1;
+    for (size_t i = 0; i < 8; i++) {
+        sleep_us(150);  // t_htss
+        uint32_t payload = (CommandManufacturerIDRead << 4) | (i << 1) | 1;
+        pio_sm_put_blocking(pio, statemachineId, payload);
+        uint32_t response = pio_sm_get_blocking(pio, statemachineId);
+        printf("address:%d ack:%d\n", i, response);
+        sleep_us(150);  // t_htss
+
+        if (response == ACK) {
+            deviceAddress = i;
+            break;
+        }
+    }
+
+    if (deviceAddress < 0) {
+        printf("No device found\n");
+        while (true) {
+            sleep_ms(1000);
+        }
+    }
+    printf("Device found at address %d!\n", deviceAddress);
 
     while (true) {
         sleep_ms(1000);
