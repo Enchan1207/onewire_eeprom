d461048409390d17c9ba672a3a6a2faf6a26c8bc
Enchan <enchant0130@gmail.com>
2024-09-14 14:00:24 +0900

    [Modify] function I/F
    - デバイスのリセットとコマンド送信,デバイス検索を関数に切り出し

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <hardware/clocks.h>
 #include <hardware/pio.h>
 #include <pico/stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -9,10 +10,28 @@
 
 const unsigned int IO_PIN = 14;
 
-const uint8_t ACK = 0;
-const uint8_t NACK = 1;
-
 const uint8_t CommandManufacturerIDRead = 0x0C;
+
+/**
+ * @brief 接続されているデバイスをリセットする
+ *
+ * @return デバイスが応答したかどうか
+ */
+bool resetDevice(PIO, uint);
+
+/**
+ * @brief デバイスにコマンドを送信する
+ *
+ * @return ACK
+ */
+bool sendCommand(PIO, uint, uint8_t);
+
+/**
+ * @brief デバイスを検索する
+ *
+ * @return int 発見できたデバイスのスレーブアドレス (-1: 見つからない)
+ */
+int searchDevice(PIO, uint);
 
 int main() {
     stdio_init_all();
@@ -26,42 +45,49 @@ int main() {
     pio_sm_set_enabled(pio, statemachineId, true);
 
     // リセット要求を送り、結果を待つ
-    pio_sm_put_blocking(pio, statemachineId, 0xFFFFFFFF);
-    uint32_t resetResult = pio_sm_get_blocking(pio, statemachineId);
-    if (resetResult != 0) {
-        printf("EEPROM Reset failed\n");
-        while (true) {
-            sleep_ms(1000);
-        }
+    bool resetResult = resetDevice(pio, statemachineId);
+    if (!resetResult) {
+        printf("Failed to reset device.\n");
+        return 1;
     }
-    printf("EEPROM Reset successfully\n");
+    printf("EEPROM Reset successfully.\n");
 
-    // オペコード 0x0C(チップ情報読み取り) を各スレーブアドレスに対して送信し、デバイスを探す
+    // 接続されているデバイスを検索
     printf("Looking for connected devices...\n");
-    int deviceAddress = -1;
-    for (size_t i = 0; i < 8; i++) {
-        sleep_us(150);  // t_htss
-        uint32_t payload = (CommandManufacturerIDRead << 4) | (i << 1) | 1;
-        pio_sm_put_blocking(pio, statemachineId, payload);
-        uint32_t response = pio_sm_get_blocking(pio, statemachineId);
-        printf("address:%d ack:%d\n", i, response);
-        sleep_us(150);  // t_htss
-
-        if (response == ACK) {
-            deviceAddress = i;
-            break;
-        }
-    }
-
+    int deviceAddress = searchDevice(pio, statemachineId);
     if (deviceAddress < 0) {
         printf("No device found\n");
-        while (true) {
-            sleep_ms(1000);
-        }
+        return 1;
     }
-    printf("Device found at address %d!\n", deviceAddress);
+    printf("Device found at address %d.\n", deviceAddress);
 
     while (true) {
         sleep_ms(1000);
     }
+}
+
+bool resetDevice(PIO pio, uint statemachineId) {
+    pio_sm_put_blocking(pio, statemachineId, 0xFFFFFFFF);
+    return pio_sm_get_blocking(pio, statemachineId) == 0;
+}
+
+bool sendCommand(PIO pio, uint statemachineId, uint8_t command) {
+    pio_sm_put_blocking(pio, statemachineId, command);
+    return pio_sm_get_blocking(pio, statemachineId) == 0;
+}
+
+int searchDevice(PIO pio, uint statemachineId) {
+    int deviceAddress = -1;
+    for (size_t i = 0; i < 8; i++) {
+        sleep_us(150);  // t_htss
+        uint32_t payload = (CommandManufacturerIDRead << 4) | (i << 1) | 1;
+        bool response = sendCommand(pio, statemachineId, payload);
+        sleep_us(150);  // t_htss
+
+        if (response) {
+            deviceAddress = i;
+            break;
+        }
+    }
+    return deviceAddress;
 }
